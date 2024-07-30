7f82a4884909a3eab78828a9f92a0bc93202cf37
Tom Ordille <thordill@microsoft.com>
2024-07-30 07:42:54 -0400

    Add project files.

---
+++ BlazorAuthServerSample/MyClaimsTransformation.cs
@@ -0,0 +1,25 @@
+﻿
+    using Microsoft.AspNetCore.Authentication;
+    using System.Security.Claims;
+
+namespace BlazorAuthServerSample
+{
+    public class MyClaimsTransformation : IClaimsTransformation
+    {
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+           
+            // get user name 
+           var userName = principal.Identity?.Name;
+           
+
+            if (principal.Identity is ClaimsIdentity claimsIdentity)
+            {
+                claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "AdminRole"));
+                claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "Role1"));
+           }
+
+            return Task.FromResult(principal);
+        }
+    }
+}
---
+++ BlazorAuthServerSample/Program.cs
@@ -0,0 +1,65 @@
+using BlazorAuthServerSample.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using System.Security.Claims;
+
+namespace BlazorAuthServerSample
+{
+    public class Program
+    {
+ 
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services
+                .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(
+                    builder.Configuration.GetSection("AzureAd"));
+
+            builder.Services
+                .AddAuthorization(
+                    policy => policy.FallbackPolicy = policy.DefaultPolicy);
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddRazorPages();
+
+            builder.Services
+                .AddServerSideBlazor()
+                .AddMicrosoftIdentityConsentHandler();
+
+            builder.Services
+                .AddControllersWithViews()
+                .AddMicrosoftIdentityUI();
+
+            builder.Services.AddTransient<IClaimsTransformation, MyClaimsTransformation>();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization(); 
+
+   
+            app.MapControllers();
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+            app.Run();
+        }
+    }
+}
